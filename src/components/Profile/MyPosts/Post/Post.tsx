75393205f6c68550c5488a00e30289321e37f9de
IvanMinets <minetssmash@gmail.com>
2023-03-29 21:56:15 +0300

    Changes

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import a from './Post.module.css';
+
+const Post = () => {
+    return (
+        <div>
+            <div className={a.item}>
+                <img src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3" alt="#"/>
+                Post 1
+            </div>
+            <div>
+                <span>like</span>
+            </div>
+        </div>
+    )
+}
+export default Post;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import a from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={a.content}>
+            <div>
+                <img
+                    src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+                    alt='#'/>
+            </div>
+            <div className={a.item}>
+                ava + description
+            </div>
+            <MyPosts/>
+        </div>
+    )
+}
+export default Profile;
