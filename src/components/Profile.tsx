157faf1931200a4d7edeff280be79269eb757a5c
Ivan Minets2 <minetssmash@gmail.com>
2023-03-29 16:38:26 +0300

    Adding css modules

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import './Profile.css';
+import a from './Profile.module.css';
 
 const Profile = () => {
     return (
-            <div className='content'>
+            <div className={a.content}>
                 <div>
                     <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' alt='#'/>
                 </div>
-                <div>
+                <div className={a.item}>
                     ava + description
                 </div>
-                <div>
+                <div  className={a.item}>
                     My posts
-                    <div>
+                </div>
+                    <div className={a.item}>
                         New Post
                     </div>
-                    <div>
+                    <div className={a.item}>
                         Post 1
                     </div>
-                    <div>
+                    <div className={a.item}>
                         Post 2
                     </div>
                 </div>
-            </div>
     )
 }
 export default Profile;
