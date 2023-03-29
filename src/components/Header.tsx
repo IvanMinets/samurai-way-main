eb338a9f33709b06123a9d7c41c275c9a4e42675
Ivan Minets2 <minetssmash@gmail.com>
2023-03-29 12:47:32 +0300

    Addded components and styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className ='header'>
+            <img src='https://i0.wp.com/bluebubbleworld.org/wp-content/uploads/2017/05/logo-blue-just-image-300x300.png?fit=300%2C300&ssl=1' alt="#"/>
+        </header>
+    )
+}
+
+export default Header;
