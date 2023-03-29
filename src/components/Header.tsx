91054b2cee4c866b0aa51cde87a2f659a004a9e8
IvanMinets <minetssmash@gmail.com>
2023-03-29 20:14:54 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Header.module.css';
+import x from './Header.module.css';
+
 
 const Header = () => {
     return (
-        <header className ='header'>
+        <header className = {x.header}>
             <img src='https://i0.wp.com/bluebubbleworld.org/wp-content/uploads/2017/05/logo-blue-just-image-300x300.png?fit=300%2C300&ssl=1' alt="#"/>
         </header>
     )
