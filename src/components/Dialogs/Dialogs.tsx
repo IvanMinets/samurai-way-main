8814d88d3bc1e426a648ae3d71cd0d3f54b0174b
IvanMinets <minetssmash@gmail.com>
2023-04-05 00:19:03 +0300

    Changing components and adding NavLinks

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
-const Dialogs = (props: any) => {
+const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/1">Ivan</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Sasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Viktor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Valera</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hi, how is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
+
     )
 }
 
