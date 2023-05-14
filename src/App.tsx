1aaaad5830d5d0d47fce09136ea0d7d0fa2cdc41
Ivan Minets <minetssmash@gmail.com>
2023-05-15 00:48:26 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import state from "./redux/state";
 
 type AppPropsType = {
     state: any
