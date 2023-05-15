e06297fc28a43c0ad4a7af0c9c11b37be133d591
Ivan Minets2 <minetssmash@gmail.com>
2023-05-15 17:37:08 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import {RootStateType} from "./redux/state";
 
+type AppPropsType = {
+    state: RootStateType
+}
 
-const App = (props: any) => {
+const App = (props: AppPropsType) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -9,6 +9,13 @@ type MyPostsPropsType = {
 const MyPosts = (props: MyPostsPropsType) => {
 
     let postsElements = props.posts.map((post)=> <Post message={post.message} likesCount={post.likesCount}/>)
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    const addPost = () => {
+        alert(newPostElement.current?.value)
+    }
+
     return (
         <div className={a.postsBlock}>
             <h3>My posts</h3>
@@ -17,7 +24,7 @@ const MyPosts = (props: MyPostsPropsType) => {
                     <textarea></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={a.posts}>
