4cfdef24cbcb8a32ada6be8422005b3a5dd8e5a7
Ivan Minets <minetssmash@gmail.com>
2023-05-28 20:21:47 +0300

    Update state.ts

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -51,7 +51,7 @@ let store : StoreType = {
                 {id: 1, message: "Hi, how are you?", likesCount: 12},
                 {id: 2, message: "It is my first post", likesCount: 11},
             ],
-            newPostText: 'it-kamasutra.com'
+            newPostText: 'it-kamasutra.com1'
         },
         dialogsPage: {
             messages: [
