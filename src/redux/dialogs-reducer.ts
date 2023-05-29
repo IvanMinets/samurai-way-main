8939c7dd617b8e62c6bf7a5694504f9ac87fe826
Ivan Minets2 <minetssmash@gmail.com>
2023-05-29 18:35:52 +0300

    Update dialogs-reducer.ts

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -1,49 +1,30 @@
 import React from "react";
-type PostType = {
-    id: number
-    message: string
-    likesCount: number
-}
+
 type SendMessageType = ReturnType<typeof sendMessageAC>
 type UpdateNewMessageType = ReturnType<typeof updateNewMessageBodyAC>
 
 type ActionsTypes = SendMessageType | UpdateNewMessageType;
 
-const dialogsReducer = (state: any, action: ActionsTypes) =>  {
-    if (action.type === 'ADD-POST') {
-        let newPost: PostType = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
-    }
-    else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-        this._state.profilePage.newPostText = action.newText;
-        this._callSubscriber(this._state);
+const dialogsReducer = (state: any, action: ActionsTypes) => {
+    if (action.type === 'SEND-MESSAGE') {
+        let body = state.newMessageBody;
+        state.newMessageBody = '';
+        state.messages.push({id: 6, message: body})
     }
     else if (action.type === 'UPDATE-NEW-MESSAGE-BODY') {
-        this._state.dialogsPage.newMessageBody = action.body;
-        this._callSubscriber(this._state);
-    }
-    else if (action.type === 'SEND-MESSAGE') {
-        let body = this._state.dialogsPage.newMessageBody;
-        this._state.dialogsPage.newMessageBody = '';
-        this._state.dialogsPage.messages.push({id: 6, message: body})
-        this._callSubscriber(this._state);
+        state.newMessageBody = action.body;
     }
 }
 
+
+export const sendMessageAC = () => {
+    return {
+        type: 'SEND-MESSAGE'
+    } as const
+}
 export const updateNewMessageBodyAC = (body: string) => {
     return {
         type: 'UPDATE-NEW-MESSAGE-BODY',
         body: body
-    } as const
-}
-export const sendMessageAC = () => {
-    return {
-        type: 'SEND-MESSAGE'
     } as const
 }
