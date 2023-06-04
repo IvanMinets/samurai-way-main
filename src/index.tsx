f0b8060fef37ddd6a7b7f9f4f9ab7db0585d85ef
IvanMinets <minetssmash@gmail.com>
2023-06-04 19:48:12 +0300

    deleted context

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './redux/redux-store';
-import StoreContext, {Provider} from "./StoreContext";
 import {BrowserRouter} from "react-router-dom";
 import {RootStateType} from "./redux/store";
+import {Provider} from "react-redux";
 
 
 
