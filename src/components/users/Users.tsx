6cc2dac5c4d8bf291f2cfb1a911320e6969b2fd7
Ivan Minets2 <minetssmash@gmail.com>
2023-06-19 19:06:23 +0300

    Update Users.tsx

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {userType} from "../../redux/users-reducer";
 import s from './users.module.css'
-import  axios from "axios";
+import axios from "axios";
+import userPhoto from '../../assets/images/user.jpg'
 
 type UsersPropsType = {
     users: Array<any>
@@ -11,22 +12,32 @@ type UsersPropsType = {
 }
 
 const Users = (props: UsersPropsType) => {
-    if (props.users.length === 0) {
-        debugger
-     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {props.setUsers(response.data.items)});
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
     return (
+
         <div>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photos.small != null ? u.photos.small : ""} alt="#" className={s.userPhoto}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="#"
+                                 className={s.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                                ? <button onClick={() => {
+                                    props.unfollow(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(u.id)
+                                }}>Follow</button>}
                         </div>
                     </span>
                     <span>
