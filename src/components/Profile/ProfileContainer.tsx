f0e7cc4e24a5ca7d27f7e43a2707f59ff78dc348
Ivan Minets2 <minetssmash@gmail.com>
2023-06-26 15:20:40 +0300

    Update ProfileContainer.tsx

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import s from './Profile.module.css'
-import MyPosts from "./MyPosts/MyPosts";
-import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {ActionsTypes} from "../../redux/store";
-import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import Profile from "./Profile";
 
-
-type ProfilePropsType = {
-
+class ProfileContainer extends React.Component {
+    render() {
+        return (
+            <Profile/>
+        )
+    }
 }
 
-const Profile = (props: ProfilePropsType) => {
-
-    return (
-        <div className={s.hello}>
-            <ProfileInfo/>
-            <MyPostsContainer  />
-        </div>
-    )
-}
-export default Profile;
+export default ProfileContainer;
