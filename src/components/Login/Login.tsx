d660f91c7bd777bb69cdfd25487bda22938fca12
Ivan Minets <minetssmash@gmail.com>
2023-07-16 00:11:11 +0300

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+export const LoginForm = (props: any) => {
+    return <form action="">
+        <div>
+            <input placeholder={"login"} type="text"/>
+        </div>
+        <div>
+            <input placeholder={"login"} type="text"/>
+        </div>
+        <div>
+            <input type="checkbox"/> remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+
+}
+
+
 export const Login = (props: any) => {
     return <div>
         <h1>Login</h1>
