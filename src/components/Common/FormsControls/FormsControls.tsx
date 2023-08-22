54b9006f88ddd02a0cc22a9d4a48011b070c553e
Ivan Minets2 <minetssmash@gmail.com>
2023-08-22 18:14:45 +0300

    bug fixing

--- src/components/Common/FormsControls/FormsControls.tsx
+++ src/components/Common/FormsControls/FormsControls.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import styles from './FormsControls.module.css'
 
-export const Textarea = ({input, meta, ...props}: any) => {
+export const FormControl = ({input, meta, child, ...props}: any) => {
     const hasError =  meta.error && meta.touched;
     return (
         <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
             <div>
-                <textarea {...input} {...props} />
+                {props.children}
             </div>
-            {hasError && <span>"some error"</span> }
+            {hasError && <span>{meta.error}</span> }
         </div>
     )
 }
-export const Input = ({input, meta, ...props}: any) => {
-    const hasError =  meta.error && meta.touched;
-    return (
-        <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {hasError && <span>"some error"</span> }
-        </div>
-    )
+
+export const Textarea = (props: any) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}>
+        <textarea {...input} {...restProps}/>
+    </FormControl>
+}
+export const Input = (props: any) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}>
+        <input {...input} {...restProps}/>
+    </FormControl>
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import {Field, reduxForm} from "redux-form";
+import {Input} from "../Common/FormsControls/FormsControls";
+import {required} from "../../utils/validators";
 
 export const LoginForm = (props: any) => {
     return (
     <form onSubmit={props.handleSubmit}>
         <div>
-            <Field placeholder={"login"} name={"login"} component={"input"} />
+            <Field placeholder={"login"} name={"login"} validate={[required]} component={Input} />
         </div>
         <div>
-            <Field placeholder={"password"} name={"password"} component={"input"} />
+            <Field placeholder={"password"} name={"password"} validate={[required]} component={Input} />
         </div>
         <div>
-            <Field component={"input"} name={"rememberMe"} type="checkbox"/> remember me
+            <Field component={Input} name={"rememberMe"} type="checkbox"/> remember me
         </div>
         <div>
             <button>Login</button>
