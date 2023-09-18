4aa1bdd533542965bb7e3a8233047c4f27b0ebf5
Ivan Minets2 <minetssmash@gmail.com>
2023-09-18 14:42:44 +0300

    Update profile-reducer.test.ts

---
+++ src/redux/profile-reducer.test.ts
@@ -0,0 +1,12 @@
+import profileReducer, {addPostAC} from "./profile-reducer";
+
+it('new post should be added', ()=>{
+    let action = addPostAC("123test");
+    let state = {
+        posts: [
+            {id: 1, message: "Hi, how are you?", likesCount: 12},
+            {id: 2, message: "It is my first post", likesCount: 11},
+        ]
+    }
+    let newState = profileReducer(state,action);
+});
